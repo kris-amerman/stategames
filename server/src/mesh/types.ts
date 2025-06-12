3cfc83f823b0f74bbd146cc8d248d9f56bcd48fc
Kristoffer Amerman <krisamerman@gmail.com>
2025-06-11 23:31:57 -0700

    small changes pre 2

--- server/src/mesh/types.ts
+++ server/src/mesh/types.ts
@@ -1,25 +1,27 @@
-export type MapSize = "small" | "medium" | "large" | "xl";
+// TODO: This will be deprecated. Use the new server/src/types.ts file.
 
-export type MeshData = {
-  allVertices: Float64Array;
-  cellOffsets: Uint32Array;
-  cellVertexIndices: Uint32Array;
-  cellNeighbors: Int32Array;
-  cellTriangleCenters: Float64Array;
-  cellCount: number;
-};
+// export type MapSize = "small" | "medium" | "large" | "xl";
 
-export interface SerializedMeshData {
-  allVertices: number[];
-  cellOffsets: number[];
-  cellVertexIndices: number[];
-  cellNeighbors: number[];
-  cellTriangleCenters: number[];
-  cellCount: number;
-}
+// export type MeshData = {
+//   allVertices: Float64Array;
+//   cellOffsets: Uint32Array;
+//   cellVertexIndices: Uint32Array;
+//   cellNeighbors: Int32Array;
+//   cellTriangleCenters: Float64Array;
+//   cellCount: number;
+// };
 
-export interface MeshConfig {
-  width: number;
-  height: number;
-  radiusOptions: Record<MapSize, number>;
-}
+// export interface SerializedMeshData {
+//   allVertices: number[];
+//   cellOffsets: number[];
+//   cellVertexIndices: number[];
+//   cellNeighbors: number[];
+//   cellTriangleCenters: number[];
+//   cellCount: number;
+// }
+
+// export interface MeshConfig {
+//   width: number;
+//   height: number;
+//   radiusOptions: Record<MapSize, number>;
+// }
